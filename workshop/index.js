8b2a093af0de7d3ff9218332f4a06c913c4fcbf8
oliverjam <oliverjamesphillips@gmail.com>
2020-02-25 18:15:15 +0000

    Write initial workshop

---
+++ workshop/index.js
@@ -0,0 +1,29 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+function calculate(a, sign, b) {
+  switch (sign) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      return "Please enter a valid sign (+, -, *, /)";
+  }
+}
+
+document.querySelector("form").addEventListener("submit", event => {
+  event.preventDefault();
+  const a = event.target.elements.a.value;
+  const sign = event.target.elements.sign.value;
+  const b = event.target.elements.b.value;
+  const answer = calculate(a, sign, b);
+  document.querySelector("#result").textContent = answer;
+  event.target.reset();
+});
